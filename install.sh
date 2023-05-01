4219ce271ac8672c1d1fe71b1d55949e204d6118
Sean O'Leary <seamicole@gmail.com>
2023-05-01 14:15:16 +0800

    Add AWS CLI and Docker to install.sh

--- install.sh
+++ install.sh
@@ -208,11 +208,42 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 && sudo apt install gh -y
 
 # ┌─────────────────────────────────────────────────────────────────────────────────────
+# │ INSTALL AWS CLI
+# └────────────────────────────────────────────────────────────────────────────────────
+
+# Install AWS CLI
+sudo apt install awscli -y
+
+# ┌─────────────────────────────────────────────────────────────────────────────────────
 # │ INSTALL HEROKU CLI
 # └────────────────────────────────────────────────────────────────────────────────────
 
 # Install Heroku CLI
 curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+
+# ┌─────────────────────────────────────────────────────────────────────────────────────
+# │ INSTALL DOCKER
+# └────────────────────────────────────────────────────────────────────────────────────
+
+# Install Docker dependencies
+sudo apt install ca-certificates gnupg -y
+
+# Add Docker's official GPG key
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Set up the repository
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the package index
+sudo apt update
+
+# Install Docker Engine, containerd, and docker-compose
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # ┌─────────────────────────────────────────────────────────────────────────────────────
 # │ AUTOREMOVE PACKAGES
